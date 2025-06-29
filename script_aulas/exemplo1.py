65d833021b37488f71e169bf8e417bd28c59740e
amandacoiado <135606303+amandacoiado@users.noreply.github.com>
2025-06-29 12:53:28 -0300

    Add files via upload

---
+++ script_aulas/exemplo1.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+ordem_execucao = []
+numero_celula = 1
+type(ordem_execucao)
+
+
+# In[2]:
+
+
+numero_celula = numero_celula + 1
+ordem_execucao.append(numero_celula)
+print(ordem_execucao)
+
+
+# In[3]:
+
+
+numero_celula = numero_celula + 1
+ordem_execucao.append(numero_celula)
+print(ordem_execucao)
+
+
+# In[4]:
+
+
+numero_celula = numero_celula + 1
+ordem_execucao.append(numero_celula)
+print(ordem_execucao)
+
+
+# In[5]:
+
+
+numero_celula = numero_celula + 1
+ordem_execucao.append(numero_celula)
+print(ordem_execucao)
+
+
+# In[6]:
+
+
+numero_celula = numero_celula + 1
+ordem_execucao.append(numero_celula)
+print(ordem_execucao)
+
+
+# In[7]:
+
+
+numero_celula = numero_celula + 1
+ordem_execucao.append(numero_celula)
+print(ordem_execucao)
+
+
+# In[8]:
+
+
+numero_celula = numero_celula + 1
+ordem_execucao.append(numero_celula)
+print(ordem_execucao)
+
+
+# In[9]:
+
+
+numero_celula = numero_celula + 1
+ordem_execucao.append(numero_celula)
+print(ordem_execucao)
+
+
+# In[10]:
+
+
+numero_celula = numero_celula + 1
+ordem_execucao.append(numero_celula)
+print(ordem_execucao)
+
+
+# In[ ]:
+
+
+
+
